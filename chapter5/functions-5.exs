b09dfcd36beef4df5e399c3649318ee8831f65b9
sytong <tongsinyin@gmail.com>
2016-07-07 13:12:08 +0800

    chapter 5 exercise 5

---
+++ chapter5/functions-5.exs
@@ -0,0 +1,7 @@
+#Use the & ... notation to rewrite the following.
+
+#– Enum.map [1,2,3,4], fn x -> x + 2 end
+IO.inspect Enum.map [1,2,3,4], &(&1 + 2)
+
+#– Enum.each [1,2,3,4], fn x -> IO.inspect x end
+Enum.each [1,2,3,4], &(IO.inspect &1)
