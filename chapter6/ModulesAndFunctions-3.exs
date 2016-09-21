44349c510b138e8bbea635fa93dd6871b9bfd5ad
sytong <tongsinyin@gmail.com>
2016-09-21 14:07:30 +0800

    chapter 6 wip

---
+++ chapter6/ModulesAndFunctions-3.exs
@@ -0,0 +1,15 @@
+# Add a quadruple function. (Maybe it could call the double function)
+
+defmodule Times do
+  def double(n) do
+    n * 2
+  end
+
+  def triple(n) do
+    n * 3
+  end
+
+  def quadruple(n) do
+    double double n
+  end
+end
